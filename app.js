71f058888fb226a87d756e38639fe1cabeef6ed1
mayura1996 <mmayurapavan@gmail.com>
2020-12-22 01:43:55 +0530

    signup done

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const request = require("request");
 
 const app=express();
 
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true})); //use body parser
 
 app.get("/",function(req,res){
-  res.send("Listening");
-})
+  res.sendFile(__dirname+"/signup.html");
+});
 
+app.post("/",function(req,res){
+  var firstName=req.body.fName;
+  var secondName=req.body.lName;
+  var email=req.body.email;
+})
 app.listen(3000,function(){
   console.log("Server is listening to port 3000")
 })
