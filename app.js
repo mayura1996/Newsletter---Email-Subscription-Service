c1578559960fe679027bd72c36fe18b1feb3d746
mayura1996 <mmayurapavan@gmail.com>
2020-12-22 16:17:02 +0530

    MailChimp connected+Login & Success pages done

--- app.js
+++ app.js
@@ -1,56 +1,67 @@
 //jshint esversion:6
 
-const express= require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const https =require("https");
-const app=express();
+const https = require("https");
+const app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:true})); //use body parser
+app.use(bodyParser.urlencoded({
+  extended: true
+})); //use body parser
 
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/signup.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",function(req,res){
-  const firstName=req.body.fName;
-  const secondName=req.body.lName;
-  const email=req.body.email;
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const secondName = req.body.lName;
+  const email = req.body.email;
 
-  var data={
-    members:[
-      {
-        email_address:email,
-        status:"subscribed",
-        merge_fields :{
-          FNAME : firstName,
-          LNAME:secondName,
-        }
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: secondName,
       }
-    ]
+    }]
   };
-  var jsonData=JSON.stringify(data);
+  var jsonData = JSON.stringify(data);
 
-const url = "https://us7.api.mailchimp.com/3.0/lists/12638d0d0f";
+  const url = "https://us7.api.mailchimp.com/3.0/lists/12638d0d0f";
 
-const options ={
-  method: "POST",
-  auth : "mayuraP:b522009ef405bfdca8dccd4cddce894b-us7"
-}
+  const options = {
+    method: "POST",
+    auth: "mayuraP:"
+  }
 
-const request = https.request(url,options,function(response){
-  response.on("data",function(data){
-    console.log(JSON.parse(data));
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
   })
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
 })
 
-request.write(jsonData);
-request.end();
-});
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Server is listening to port 3000")
 })
 
